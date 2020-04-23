c395e6ff14f70d381579277a90155066039f149f
BSPuppy <405932685@qq.com>
2020-04-23 18:06:05 +0800

    test

---
+++ src/main/java/com/fifth_fleet/jagras/JagrasApplication.java
@@ -0,0 +1,15 @@
+package com.fifth_fleet.jagras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagrasApplication {
+
+    public static void main(String[] args) {
+
+        //SpringApplication.run(JagrasApplication.class, args);
+        System.out.println("Hello MHW");
+    }
+
+}
---
+++ src/test/java/com/fifth_fleet/jagras/JagrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fifth_fleet.jagras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JagrasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
