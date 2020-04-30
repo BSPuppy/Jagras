61ad31ee8202831e61b912ee16dc08cff20508b6
BSPuppy <405932685@qq.com>
2020-04-30 10:49:27 +0800

    Init Springboot

---
+++ src/main/java/com/fifthfleet/jagras/JagrasApplication.java
@@ -0,0 +1,13 @@
+package com.fifthfleet.jagras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JagrasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JagrasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fifthfleet/jagras/JagrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fifthfleet.jagras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JagrasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
